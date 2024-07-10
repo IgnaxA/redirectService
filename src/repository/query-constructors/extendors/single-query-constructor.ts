ec8b12330b127678ad5e5706f90fe1d99adf2457
IgnaxA <114932439+IgnaxA@users.noreply.github.com>
2024-07-11 01:14:38 +0500

    feature: added migration and transaction runners, .env parser for db info and query constructor (#3)

---
+++ src/repository/query-constructors/extendors/single-query-constructor.ts
@@ -0,0 +1,15 @@
+import {QueryConstructor} from "../query-constructor";
+
+export class SingleQueryConstructor extends QueryConstructor {
+    constructor() {
+        super();
+    }
+
+    public setQuery(body: string): void {
+        this.query = body;
+    }
+
+    public setParameters(parameters: Array<any>): void {
+        this.parameters = parameters;
+    }
+}
